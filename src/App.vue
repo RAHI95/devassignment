2a805721bc5005af71ae2cc49f5dca87ab3070f7
RAHI95 <65624888+RAHI95@users.noreply.github.com>
2022-09-13 19:50:21 +0600

    sidebar & accordion

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+// import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
+import Accordion from './components/accordion.vue';
+export default {
+  components: {
+    Sidebar,
+    Accordion
+},
+
+}
+</script>
+
+<template>
+  <div id="root">
+    <Sidebar/>
+    <Accordion/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  #root {
+    display: grid;
+    grid-template-areas: 'sidebar router-view';
+    grid-template-columns: auto 1fr;
+    font-family: sans-serif;
+  }
+  .sidebar {
+    grid-area: 'sidebar';
+  }
+  .router-view {
+    grid-area: 'router-view';
+    padding: 1rem;
+  }
+  </style>
