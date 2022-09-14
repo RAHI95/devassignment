9df0d46e04bf17d2f64e40f8999741d6f064ecc5
RAHI95 <65624888+RAHI95@users.noreply.github.com>
2022-09-14 19:16:36 +0600

    finished

--- src/components/accordion.vue
+++ src/components/accordion.vue
@@ -1,62 +1,27 @@
 <template>
   <div>
     <div id="">
-      
       <div class="accordion" id="accordionExample">
-        <transition name="button" mode="out-in">
-          <div class="accordion-item"  v-if="!showOnButton">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="d-flex bg-white mx-auto">
-                <a @click.prevent="onShowButton()">User</a>
-                <a
-                  href="#"
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseOne"
-                  aria-expanded="false"
-                  aria-controls="collapseOne"
-                ></a>
-              </button>
-            </h2>
-          </div>
-          <div class="accordion-item below"  v-else>
-            <h2 class="accordion-header" id="headingOne">
-              <button class="d-flex bg-white mx-auto">
-                <a @click.prevent="onShowButton()">User</a>
-                <a
-                  href="#"
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseOne"
-                  aria-expanded="false"
-                  aria-controls="collapseOne"
-                ></a>
-              </button>
-            </h2>
-          </div>
-        </transition>
-        <!-- <transition name="button" mode="out-in"> -->
-          <div class="accordion-item"  v-if="!showOnButton">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="d-flex bg-white mx-auto">
-                <a 
-                @click.prevent="onShowButton()" >View</a>
-                <a
-                  href=""
-                  class="accordion-button"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseTwo"
-                  aria-expanded="true"
-                  aria-controls="collapseTwo"
-                ></a>
-              </button>
-            </h2>
-          </div>
-        <!-- </transition> -->
-        
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <transition-group tag="ul">
+              <li v-for="item in list" :key="item.name">
+                <button class="d-flex bg-white mx-auto">
+                  <a @click="reorder">{{ item.name }}</a>
+                  <a
+                    href="#"
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    :data-bs-target="`#${item.id}`"
+                    aria-expanded="false"
+                    :aria-controls="`${item.id}`"
+                  ></a>
+                </button>
+              </li>
+            </transition-group>
+          </h2>
+        </div>
         <div>
           <div
             id="collapseOne"
@@ -67,9 +32,7 @@
             <div class="accordion-body">
               <strong>User Content</strong>
               <p>
-                It is shown by default, until the collapse plugin adds the
-                appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing
+                as well as the showing
                 and hiding via CSS transitions. You can modify any of this with
                 custom CSS or overriding our default variables. It's also worth
                 noting that just about any HTML can go within the though the
@@ -97,9 +60,7 @@
             </div>
           </div>
         </div>
-
       </div>
-   
     </div>
   </div>
 </template>
@@ -108,41 +69,55 @@
 export default {
   data() {
     return {
-      showOnButton: false,
-
+      list: [
+        {
+          name: "User",
+          id: "collapseOne",
+        },
+        {
+          name: "view",
+          id: "collapseTwo",
+        },
+      ],
     };
   },
-  computed: {
-    transition() {
-      return this.isSlide;
-    },
-  },
+
   methods: {
     onShowButton() {
-            this.showOnButton = !this.showOnButton;
-        },
-   
+      this.showOnButton = !this.showOnButton;
+    },
+    reorder() {
+      this.list.unshift(this.list[this.list.length - 1]);
+      this.list.splice(this.list.length - 1, 1);
+    },
   },
 };
 </script>
-
 <style>
-  .below{
-    position: relative;
-    top:4rem
-  }
- .button-enter-from,
-button-leave-to {
-    /* opacity: 0; */
-    transform: translateY(60px);
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
-.button-enter-to,
-.button-leave-from {
-    /* opacity: 1; */
-    transform: translateY(0px);
+button {
+  display: block;
 }
-.button-enter-active,
-.button-leave-active {
-    transition: all 1s;
+
+li {
+  transition: 0.5s;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s;
+}
+
+.v-leave-active {
+  position: absolute;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
