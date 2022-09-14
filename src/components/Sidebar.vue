8a4b9c39833fe112f82d6f1f91cbbc31e4630a0c
RAHI95 <65624888+RAHI95@users.noreply.github.com>
2022-09-14 14:26:35 +0600

    animation on button

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -41,84 +41,23 @@ aside {
 aside[vue\:is-open="true"] {
   width: 3 * $sidebar-width;
 }
-ul {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  padding-block-end: 1rem;
-}
-img {
-  object-fit: contain;
-}
+
 li {
   min-width: fit-content;
   cursor: pointer;
   padding-inline-start: $sidebar-padding-inline-start;
-  &:hover {
-    color: clr.$secondary;
-    background-color: lighten($color: clr.$bg-dark, $amount: 5);
+  
   }
-  & a {
-    // border-right: 0.25rem solid white;
-    display: flex;
-    align-items: center;
-    column-gap: 0.75rem;
-    position: relative;
-    padding-block: 0.5rem;
-  }
-  a.router-link-exact-active::after {
-    content: "";
-    position: absolute;
-    right: 0;
-    width: 0.25rem;
-    height: 100%;
-    background-color: clr.$secondary;
-  }
-}
 .sidebar-head {
   position: relative;
   padding-left: 0 !important;
-  // padding-block-end: 4rem;
-}
-.sidebar-toggle {
-  padding-inline-start: $sidebar-padding-inline-start;
 }
 h4 {
-  padding-block-end: 1rem;
-  padding-inline-start: $sidebar-padding-inline-start;
-  user-select: none;
-  pointer-events: none;
   opacity: 0.5;
-  text-transform: uppercase;
-  font-size: 0.75rem;
-  letter-spacing: 0.125ch;
   transition: opacity $toggle-duration;
 }
 h4[transparent="true"] {
   opacity: 0;
 }
-button {
-  cursor: pointer;
-  position: absolute;
-  transition-duration: $toggle-duration;
-  transition-property: transform, left, top;
-  left: 0;
-  top: 1rem;
-  transform: translateX(0%) translateY(2rem) rotateZ(0deg);
-  &.toggle-button {
-    left: 100%;
-    top: 0;
-    transform: translateX(-100%) translateY(0rem) rotateZ(180deg);
-  }
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition-property: opacity, transform;
-  transition-duration: $toggle-duration;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-  transform: translateX(-100%);
-}
+
 </style>
--- src/components/accordion.vue
+++ src/components/accordion.vue
@@ -1,89 +1,138 @@
 <template>
   <div>
-     <div id="">
+    <div id="">
+      
       <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="d-flex bg-white mx-auto">
-        <a @click="leaveScreen = true" class="click-me">User</a>
-        <a href="#" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"></a>
-      </button>
-    </h2>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="d-flex bg-white mx-auto">
-        <a @click="toggleTheme">View</a>
-        <a href="" class="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"></a>
-      </button>
-    </h2>
-  </div>
-  <div>
-    <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>User Content</strong>
-         <p>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.</p> 
+        <transition name="button" mode="out-in">
+          <div class="accordion-item"  v-if="!showOnButton">
+            <h2 class="accordion-header" id="headingOne">
+              <button class="d-flex bg-white mx-auto">
+                <a @click.prevent="onShowButton()">User</a>
+                <a
+                  href="#"
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="false"
+                  aria-controls="collapseOne"
+                ></a>
+              </button>
+            </h2>
+          </div>
+          <div v-else class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+              <button class="d-flex bg-white mx-auto">
+                <a 
+                @click.prevent="onShowButton()" >View</a>
+                <a
+                  href=""
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="true"
+                  aria-controls="collapseTwo"
+                ></a>
+              </button>
+            </h2>
+          </div>
+        </transition>
+        <div>
+          <div
+            id="collapseOne"
+            class="accordion-collapse collapse"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <strong>User Content</strong>
+              <p>
+                It is shown by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the though the
+                transition does limit overflow.
+              </p>
+            </div>
+          </div>
+          <div
+            id="collapseTwo"
+            class="accordion-collapse collapse show"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <strong>View Content</strong>
+              <p>
+                It is shown by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the though the
+                transition does limit overflow.
+              </p>
+            </div>
+          </div>
+        </div>
+
       </div>
+   
     </div>
-    <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>View Content</strong> 
-        <p>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.</p> 
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-    <transition name="leave-screen">
-    <div v-if="leaveScreen" class="leave-screen" @click="leaveScreen = false"></div>
-  </transition>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-        return{
-          leaveScreen: false
-        }
+  data() {
+    return {
+      showOnButton: false,
+
+    };
+  },
+  computed: {
+    transition() {
+      return this.isSlide;
     },
-    methods:{
-      toggleTheme(){
-        // alert('clicked')
-          this.isActive = !this.isActive;
-      }
-    }
+  },
+  methods: {
+    onShowButton() {
+            this.showOnButton = !this.showOnButton;
+        },
+    shouldSlide() {
+      this.isSlide = true;
+      this.show = false;
+      setTimeout(() => {
+        this.show = true;
+      }, 0);
+    },
+    shouldFade() {
+      this.isSlide = false;
+      this.show = false;
+      setTimeout(() => {
+        this.show = true;
+      }, 0);
+    },
+  },
 };
 </script>
 
 <style>
-  body {
-  margin: 0;
+ .button-enter-from,
+button-leave-to {
+    /* opacity: 0; */
+    transform: translateY(30px);
 }
-
-.click-me {
-  cursor: pointer;
-  font-size: 30px;
+.button-enter-to,
+.button-leave-from {
+    /* opacity: 1; */
+    transform: translateY(0px);
 }
-
-.leave-screen {
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-  top: 0;
-  background-color: rgb(0, 0, 0);
+.button-enter-active,
+.button-leave-active {
+    transition: all 1s;
 }
-.leave-screen-enter-active,
-.leave-screen-leave-active {
-  background-color: rgba(0, 0, 0, 1);
-  transform: translateX(0);
-  transition: all 1s ease-in-out;
-}
-
-.leave-screen-leave-to,
-.leave-screen-enter {
-  background-color: rgba(0, 0, 0, 0);
-  transform: translateX(-100%);
-}
-
 </style>
