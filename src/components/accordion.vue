2df04a641d18de89cd29de5e3812fd95fa0e0a01
RAHI95 <65624888+RAHI95@users.noreply.github.com>
2022-09-14 12:37:29 +0600

    buttons updated

--- src/components/accordion.vue
+++ src/components/accordion.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
-     <div>
+     <div id="">
       <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        User
+      <button class="d-flex bg-white mx-auto">
+        <a @click="leaveScreen = true" class="click-me">User</a>
+        <a href="#" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"></a>
       </button>
     </h2>
-    
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        View
+      <button class="d-flex bg-white mx-auto">
+        <a @click="toggleTheme">View</a>
+        <a href="" class="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"></a>
       </button>
     </h2>
-    
   </div>
   <div>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+    <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
         <strong>User Content</strong>
          <p>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.</p> 
       </div>
     </div>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+    <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
         <strong>View Content</strong> 
         <p>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.</p> 
@@ -34,12 +34,56 @@
   </div>
 </div>
     </div>
+    <transition name="leave-screen">
+    <div v-if="leaveScreen" class="leave-screen" @click="leaveScreen = false"></div>
+  </transition>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+        return{
+          leaveScreen: false
+        }
+    },
+    methods:{
+      toggleTheme(){
+        // alert('clicked')
+          this.isActive = !this.isActive;
+      }
+    }
+};
 </script>
 
 <style>
+  body {
+  margin: 0;
+}
+
+.click-me {
+  cursor: pointer;
+  font-size: 30px;
+}
+
+.leave-screen {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  top: 0;
+  background-color: rgb(0, 0, 0);
+}
+.leave-screen-enter-active,
+.leave-screen-leave-active {
+  background-color: rgba(0, 0, 0, 1);
+  transform: translateX(0);
+  transition: all 1s ease-in-out;
+}
+
+.leave-screen-leave-to,
+.leave-screen-enter {
+  background-color: rgba(0, 0, 0, 0);
+  transform: translateX(-100%);
+}
+
 </style>
---
+++ src/components/accordion2.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <div class="d-grid gap-2">
+        <button
+          type="button"
+          class="btn btn-success mt-3"
+          style="height: auto"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapse-b1"
+          aria-expanded="false"
+          aria-controls="collapse-b1"
+        >
+        <i class="bi bi-arrow-down"></i>
+          User
+        </button>
+      </div>
+  
+      <div class="d-grid gap-2">
+        <button
+          type="button"
+          class="btn btn-primary mt-1"
+          style="height: auto"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapse-b2"
+          aria-expanded="false"
+          aria-controls="collapse-b2"
+        >
+          View
+        </button>
+      </div>
+  
+      <div id="myGroup">
+        <!-- Add the parent id here -->
+  
+        <div class="collapse" id="collapse-b1" data-bs-parent="#myGroup">
+          <div class="alert alert-success mt-3">User Content</div>
+        </div>
+  
+        <div class="collapse" id="collapse-b2" data-bs-parent="#myGroup">
+          <div class="alert alert-primary mt-3">View Content</div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style>
+  </style>
